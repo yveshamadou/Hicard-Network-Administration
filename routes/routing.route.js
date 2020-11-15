6e090aa0409b0c8eeb9dd8067d0770f79fba879f
LothKmer <landryonguene@hotmail.com>
2020-11-15 15:46:21 +0100

    Canceling of API server side
    Apis are going to be called in the client side rather

--- routes/routing.route.js
+++ routes/routing.route.js
@@ -19,7 +19,6 @@ app.use(appAuthentication) //The app must be logged in before any action
 //Routes Calling
 const authenticationRoutes = require('./authentication.route')
 const mainRoutes = require('./main.route')
-const apiRoutes = require('./api.route')
 const errorsRoutes = require('./errors.route')
 
 app.use(authenticationRoutes)
@@ -27,7 +26,6 @@ app.use(authenticationRoutes)
 /* app.use(memberIsLogged)  *///Middleware : From here, the member must be logged in to access the app below
 
 app.use(mainRoutes)
-app.use(apiRoutes)
 app.use(errorsRoutes)
 
 module.exports = app
